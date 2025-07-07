1d4b172e5833e1d5ffa9ae0423a92cd52103d670
Juan Pablo Pieroni <seisduro@gmail.com>
2025-07-07 17:08:43 -0300

    Get y Post resuelto . no funciona el import module

--- app.js
+++ app.js
@@ -1,13 +1,46 @@
+import ProductManager from "./ProductManager.js"
 const express = require("express")
 const app = express()
 const PORT = 8080
+const manager = new ProductManager("productos.json")
+app.use(express.json())
+app.get("/", (req, res) => {
+    res.status(200).send("Entrega 1 de Backend. Comision 74305")
+})
+app.get("/api/products", (req, res) => {
+    const productos = manager.getProducts()
+    res.json(productos)
+})
 
-app.get("/api/productos", (req, res) => {})
+app.get("/api/products/:pid", (req, res) => {
+    const id = parseInt(req.params.pid)
+    const producto = manager.getProductById(id)
+    if (producto) {
+        res.status(200).json(producto)
+    } else {
+        res.status(404).json({ error: "Producto no encontrado" })
+    }
+})
 
-app.get("/api/products/:pid", (req, res) => {})
+app.post("/api/products", (req, res) => {
+    const datos = req.body
+    if (
+        !datos.title ||
+        !datos.description ||
+        !datos.price ||
+        !datos.code ||
+        !datos.stock
+    ) {
+        return res.status(400).json({ error: "Faltan datos del producto" })
+    }
+    const nuevoProducto = manager.addProduct(datos)
+    res.status(201).json(nuevoProducto)
+})
+app.put("/api/products/:pid", (req, res) => {
+    
 
-app.post("/api/products", (req, res) => {})
-app.put("/api/products/:pid", (req, res) => {})
+
+})
 app.delete("/api/products/:pid", (req, res) => {})
 
 app.listen(PORT, () => {
--- productManager.js
+++ productManager.js
@@ -1,5 +1,38 @@
+import fs from "fs"
+
 class ProductManager {
     constructor(path) {
         this.path = path
     }
+    getProducts() {
+        if (!fs.existsSync(this.path)) {
+            return []
+        }
+        const contenido = fs.readFileSync(this.path, "utf-8")
+        return JSON.parse(contenido)
+    }
+
+    addProduct(producto) {
+        const productos = this.getProducts()
+        const nuevoId =
+            productos.length > 0 ? productos[productos.length - 1].id + 1 : 1
+        const nuevoProducto = { id: nuevoId, ...producto }
+        productos.push(nuevoProducto)
+        fs.writeFileSync(this.path, JSON.stringify(productos, null, 2))
+        return nuevoProducto
+    }
+
+    getProductById(id) {
+        const productos = this.getProducts()
+        return productos.find((p) => p.id === parseInt(id)) || null
+    }
 }
+// const producto1 = new ProductManager("productos.json")
+// producto1.addProduct({
+//     title: "Monitor",
+//     description: "ASUS 27",
+//     price: 100,
+//     code: "ABC123",
+//     stock: 10,
+// })
+export default ProductManager
