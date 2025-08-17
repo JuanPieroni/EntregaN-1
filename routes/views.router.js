a4082833635b16412ce2f9bb15311bd73468a0ad
Juan Pablo Pieroni <seisduro@gmail.com>
2025-08-17 02:27:42 -0300

    agrego vistas

--- routes/views.router.js
+++ routes/views.router.js
@@ -9,7 +9,13 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 router.get("/products", async (req, res) => {
-  
+    try {
+        const products = await productsManager.findAllProducts(req.query)
+        res.render("index", { products })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send("Error al obtener productos")
+    }
 })
 
 router.get("/carts", (req, res) => {
